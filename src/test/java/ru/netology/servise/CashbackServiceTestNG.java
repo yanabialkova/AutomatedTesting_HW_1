f64a0554d6837a540877a681eaa4c3c30ae0343f
Yana Bialkova <yana.bialkova@gmail.com>
2022-08-03 16:24:20 +0200

    Junit4

--- src/test/java/ru/netology/servise/CashbackServiceTestNG.java
+++ src/test/java/ru/netology/servise/CashbackServiceTestNG.java
@@ -1,34 +1,33 @@
-package ru.netology.servise;
-
-import org.testng.Assert;
-import org.testng.annotations.Test;
-
-
-public class CashbackServiceTestNG {
-
-    @Test
-    public void shouldPositiveResult() {
-        CashbackHackService service = new CashbackHackService();
-
-        int amount = 900;
-
-        int actual = service.remain(amount);
-        int expected = 100;
-
-       Assert.assertEquals(actual,expected);
-
-    }
-
-    @Test
-    public void shouldNegativeNumber() {
-        CashbackHackService service = new CashbackHackService();
-
-        int amount = -1000;
-
-        int actual = service.remain(amount);
-        int expected = 1000;
-
-        Assert.assertEquals(actual,expected);
-
-    }
-}
+//package ru.netology.servise;
+//
+//import org.testng.Assert;
+//import org.testng.annotations.Test;
+//
+//public class CashbackServiceTestNG {
+//
+//    @Test
+//    public void shouldPositiveResult() {
+//        CashbackHackService service = new CashbackHackService();
+//
+//        int amount = 900;
+//
+//        int actual = service.remain(amount);
+//        int expected = 100;
+//
+//       Assert.assertEquals(actual,expected);
+//
+//    }
+//
+//    @Test
+//    public void shouldNegativeNumber() {
+//        CashbackHackService service = new CashbackHackService();
+//
+//        int amount = -1000;
+//
+//        int actual = service.remain(amount);
+//        int expected = 1000;
+//
+//        Assert.assertEquals(actual,expected);
+//
+//    }
+//}
